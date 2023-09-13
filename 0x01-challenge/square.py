e932d56015278751c8678efcd7d3c8939959bfc3
MercyNJ <mercinjuguna@gmail.com>
2023-09-13 09:51:55 +0000

    Debugging a square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+""" A module that defines a class square."""
 
-class square():
+class Square:
+    """ A square class."""
     
-    width = 0
-    height = 0
-
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width = 0, height = 0):
+        """Initialization of a square instance"""
+        if width < 0 or height < 0:
+            raise ValueError("Width and height must be non-negative.")
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """Return str representation of square"""
+        return "Square {}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
