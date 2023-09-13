ab38622f79b618246105be7add0b9794f6e264e3
MercyNJ <mercinjuguna@gmail.com>
2023-09-13 11:20:07 +0000

    Debugging square code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,6 +9,36 @@ class Square():
         self.width = width
         self.height = height
 
+    @property
+    def width(self):
+        """Getter method for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width's attribue setter method"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Height attribute's getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height attributes's setter method"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
